bffc9e0d2e3bcb4a5f9739430f60b035d8453e17
Manuel Odendahl <wesen@ruinwesen.com>
2016-11-26 09:25:41 -0500

    chain tables

--- src/types.rs
+++ src/types.rs
@@ -3,25 +3,38 @@ use ast::Ty;
 
 use std::collections::HashMap;
 
-struct Table {
-    map: HashMap<SymbolId, Box<Ty>>,
+struct Table<'a, T: 'a> {
+    parent: Option<&'a Table<'a, T>>,
+    map: HashMap<SymbolId, Box<T>>,
 }
 
-impl Table {
-    fn new() -> Table {
-        Table { map: HashMap::new() }
+impl<'a, T: 'a> Table<'a, T> {
+    fn new(parent: Option<&'a Table<'a, T>>) -> Table<'a, T> {
+        Table {
+            map: HashMap::new(),
+            parent: parent
+        }
     }
 
-    fn enter(&mut self, s: SymbolId, ty: Box<Ty>) {
-        self.map.insert(s, ty);
+    fn enter(&mut self, s: SymbolId, v: Box<T>) {
+        self.map.insert(s, v);
     }
 
-    fn look(&self, s: SymbolId) -> Option<&Box<Ty>> {
-        self.map.get(&s)
+    fn look(&self, s: SymbolId) -> Option<&Box<T>> {
+        let v = self.map.get(&s);
+        match (v, self.parent.as_ref()) {
+            (Some(_), _) => v,
+            (None, Some(parent)) => parent.look(s),
+            (None, None) => None,
+        }
     }
 
     fn contains(&self, s: SymbolId) -> bool {
-        self.map.contains_key(&s)
+        match (self.map.contains_key(&s), self.parent.as_ref()) {
+            (true, _) => true,
+            (false, None) => false,
+            (false, Some(parent)) => parent.contains(s)
+        }
     }
 }
 
@@ -30,11 +43,21 @@ fn test_table() {
     use ast::Ty::*;
 
     let ty = Box::new(NameTy(0, 0));
-    let mut table = Table::new();
+    let ty2 = Box::new(NameTy(1, 1));
+
+    let mut table: Table<Ty> = Table::new(None);
     table.enter(0, ty);
     assert_eq!(table.contains(0), true);
     assert_eq!(table.contains(1), false);
     let res = table.look(0);
     assert!(res.is_some());
-    assert_eq!(&**res.unwrap(), &NameTy(0,0));
+    assert_eq!(&**res.unwrap(), &NameTy(0, 0));
+
+    let mut t2: Table<Ty> = Table::new(Some(&table));
+    t2.enter(1, ty2);
+    assert_eq!(t2.contains(0), true);
+    assert_eq!(t2.contains(1), true);
+    assert_eq!(t2.contains(2), false);
+    assert_eq!(&**t2.look(0).unwrap(), &NameTy(0,0));
+    assert_eq!(&**t2.look(1).unwrap(), &NameTy(1,1));
 }
